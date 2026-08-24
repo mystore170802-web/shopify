777d1fa11d25227b6db13cc714a9287d25d32079
mystore170802-web <mystore170802@gmail.com>
2026-08-24 09:52:09 +0700

    Update shopify-2.js

--- shopify-2.js
+++ shopify-2.js
@@ -1,10 +1,5 @@
 setInterval(() => {
-        const myInput = document.querySelector('#selling_plan'); 
-        const m1 = document.querySelector('[name=selling_plan]'); 
-        if (myInput) {
-                myInput.value = "8751468371";  
-        }   
-        if (m1) {
-                m1.value = "8751468371";  
-        }     
+        document.querySelectorAll("div[data-pf-type='Block']").forEach(e => {
+                e.remove();
+        })
     }, 200); 
