095ad92f67e752467f20da45d4a1b3fed2ee4478
mystore170802-web <mystore170802@gmail.com>
2026-08-28 10:44:35 +0700

    Update shopify-2.js

--- shopify-2.js
+++ shopify-2.js
@@ -1,5 +1,7 @@
 setInterval(() => {
-        document.querySelectorAll("div[data-pf-type='Block']").forEach(e => {
-                e.remove();
-        })
-    }, 200); 
+    document.querySelectorAll("div[data-pf-type='Block']").forEach(e => {
+        if (!e.closest('.product-quest')) {
+            e.remove();
+        }
+    });
+}, 200);
